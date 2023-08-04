f135a1917ffa7f6a644db97a32d64104cdb6778e
Sobir <sabdullaev@avanpost.ru>
2023-08-04 16:28:47 +0300

    добавлен формсет

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Dog
+from .models import Dog, Parent
 
 
 class DogForm(forms.ModelForm):
     class Meta:
         model = Dog
+        fields = '__all__'
+
+
+class ParentForm(forms.ModelForm):
+    class Meta:
+        model = Parent
         fields = '__all__'
--- dogs/views.py
+++ dogs/views.py
@@ -1,10 +1,13 @@
 from typing import Any, Dict
 from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
-from .models import Category, Dog
+from .models import Category, Dog, Parent
 from django.urls import reverse_lazy, reverse
-from .forms import DogForm
+from .forms import DogForm, ParentForm
+from django.forms import inlineformset_factory
 
 
 class IndexView(TemplateView):
@@ -55,7 +58,25 @@ class DogUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('dogs:category_dogs', args=[self.object.category.pk])
+    
+    def get_context_data(self, **kwargs: Any):
+        context_data = super().get_context_data(**kwargs)
+        ParentFormSet = inlineformset_factory(Dog, Parent, form=ParentForm, extra=1)
+        if self.request.method == 'POST':
+            formset = ParentFormSet(self.request.POST, instance=self.object)
+        else:
+            formset = ParentFormSet(instance=self.object)
+        context_data['formset'] = formset
+        return context_data
 
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
 
 class DogDeleteView(DeleteView):
     model = Dog
