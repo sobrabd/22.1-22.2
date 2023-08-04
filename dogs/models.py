5a3daab8b55f6e516fc5992a1f2ac3c3de16749c
Sobir <sabdullaev@avanpost.ru>
2023-08-04 15:44:19 +0300

    старт практики 22 урок

---
+++ dogs/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Порода')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'порода'
+        verbose_name_plural = 'породы'
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Кличка')
+    # category = models.CharField(max_length=100, verbose_name='Порода')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Порода')
+
+    phot = models.ImageField(upload_to='dogs/', null=True, blank=True, verbose_name='Фото')
+    birth_day = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'собака'
+        verbose_name_plural = 'собаки'
---
+++ dogs/views.py
@@ -0,0 +1,61 @@
+from typing import Any, Dict
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
+from .models import Category, Dog
+from django.urls import reverse_lazy, reverse
+
+
+class IndexView(TemplateView):
+    template_name = 'dogs/index.html'
+    extra_context = {
+        'title': 'Питомник - Добро пожаловать',
+    }
+
+    def get_context_data(self, **kwargs: Any):
+        context_data = super().get_context_data(**kwargs)
+        context_data['object_list'] = Category.objects.all()[:3]
+        return context_data
+
+
+class CategoryListView(ListView):
+    model = Category
+    extra_context = {
+        'title': 'Питомник - Наши породы'
+    }
+
+
+class DogListView(ListView):
+    model = Dog
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset.filter(category_id=self.kwargs.get('pk'))
+        return queryset
+    
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        category_item = Category.objects.get(pk=self.kwargs.get('pk'))
+        context_data['object_list'] = Dog.objects.filter(category_id=category_item.pk)
+        context_data['title'] = f'Все собаки породы {category_item.name}'
+
+        return context_data
+
+
+class DogCreateView(CreateView):
+    model = Dog
+    fields = ('name', 'category')
+    success_url = reverse_lazy('dogs:categories')
+
+
+class DogUpdateView(UpdateView):
+    model = Dog
+    fields = ('name', 'category')
+
+    def get_success_url(self):
+        return reverse('dogs:category_dogs', args=[self.object.category.pk])
+    
+
+class DogDeleteView(DeleteView):
+    model = Dog
+    success_url = reverse_lazy('dogs:categories')
