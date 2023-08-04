3b5c060576117efa6a5d36cb981fc9470694084b
Sobir <sabdullaev@avanpost.ru>
2023-08-04 16:34:03 +0300

    стилизация форм

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,14 +1,23 @@
 from django import forms
 from .models import Dog, Parent
+from datetime import datetime
 
 
-class DogForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class DogForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Dog
         fields = '__all__'
 
 
-class ParentForm(forms.ModelForm):
+
+class ParentForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Parent
         fields = '__all__'
