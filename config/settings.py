45066ad6392c96a9bb86f71affd6054fab672a91
Sobir <sabdullaev@avanpost.ru>
2023-08-09 14:03:37 +0300

    Добавлено приложение users

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'dogs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/users/'
---
+++ users/views.py
@@ -0,0 +1,58 @@
+import random
+
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView
+
+from users.forms import UserRegisterForm, UserForm
+from users.models import User
+from users.services import send_new_password
+
+
+class LoginView(BaseLoginView):
+    template_name = 'users/login.html'
+
+
+class LogoutView(BaseLogoutView):
+    pass
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/register.html'
+
+    def form_valid(self, form):
+        new_user = form.save()
+        # send_mail(
+        #     subject='Поздравляем в регистрацией',
+        #     message='Вы зарегистрировались на нашей платформе, добро пожаловать!',
+        #     from_email=settings.EMAIL_HOST_USER,
+        #     recipient_list=[new_user.email]
+        # )
+        return super().form_valid(form)
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    success_url = reverse_lazy('users:profile')
+    form_class = UserForm
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+@login_required
+def generate_new_password(request):
+    new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+    request.user.set_password(new_password)
+    request.user.save()
+    send_new_password(request.user.email, new_password)
+    return redirect(reverse('dogs:index'))
