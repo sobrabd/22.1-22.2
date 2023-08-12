d2e36d023ed1a24818be5c11c4b1c63be8b940d3
Sobir <sabdullaev@avanpost.ru>
2023-08-12 12:44:12 +0300

    Добавлена регистрация

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import LoginView, LogoutView
+from users.views import LoginView, LogoutView, RegisterView
 
 app_name = UsersConfig.name
 
@@ -9,5 +9,6 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-   
+    path('register/', RegisterView.as_view(), name='register'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import User
+from .forms import UserRegisterForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class LoginView(BaseLoginView):
@@ -11,3 +15,11 @@ class LoginView(BaseLoginView):
 
 class LogoutView(BaseLogoutView):
     pass
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/register.html'
+
